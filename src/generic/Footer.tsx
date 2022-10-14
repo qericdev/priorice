0c14a275a79df64355c896b8a159c69188874673
Erick Rojas <qericdev@gmail.com>
2022-10-14 18:34:51 -0500

    navbar fully responsive

---
+++ src/generic/Footer.tsx
@@ -0,0 +1,20 @@
+import styles from './../styles/generic/Footer.module.css';
+
+function Footer() : JSX.Element {
+    return (
+        <footer className={styles.footer}>
+            <section className={styles.keywords}>
+                <h3>Keywords</h3>
+            </section>
+            <section className={styles.search}>
+                <label for="site-search">Search in the site:</label>
+                <input id="site-search" type="search"></input>
+            </section>
+            <section className={styles.social}>
+
+            </section>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/generic/Navbar.tsx
@@ -0,0 +1,50 @@
+import styles from './../styles/generic/Navbar.module.css';
+import {useState, useEffect} from 'react';
+
+function Navbar() : JSX.Element {
+
+    const [menu, setMenu] = useState<boolean>(false);
+    const [isFirstLoad, setIsFirstLoad] = useState<boolean>(true);
+
+    useEffect(() => {
+        function handleResize() : void {
+            if (window.innerWidth >= 750) {
+                setIsFirstLoad(true);
+                setMenu(false);
+            }
+        }
+
+        window.addEventListener("resize", handleResize);
+
+        return () => window.removeEventListener("resize", handleResize);
+    },[])
+
+    function showMenu() : void {
+        setMenu(prevMenu => !prevMenu);
+        setIsFirstLoad(false);
+    }
+
+    return (
+        <nav id="nav" className={styles.nav}>
+            <p className={styles.logo}>PRIORICE</p>
+            <menu className={styles.tabs}>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Services</li>
+                <li>News</li>
+            </menu>
+            <button className={styles.contact}>Contact Us</button>
+            {/* responsiveMenu Class is the design for the menu in small screens, by default in Desktops it is display as none */}
+            <img onClick={showMenu} alt="menu" className={styles.burgerIconMenu} src="./../../images/generic/menu-icon.png"/>
+            <menu className={menu ? styles.responsiveMenuOn : isFirstLoad ? styles.responsiveMenuOffFirstLoad : styles.responsiveMenuOff}>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Services</li>
+                <li>News</li>
+                <li>Contact Us</li>
+            </menu>
+        </nav>
+    );
+}
+
+export default Navbar;
