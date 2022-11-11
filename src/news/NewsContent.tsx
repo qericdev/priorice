d5c6e63ebe81685a6283c34b1f84ef485ee51671
Erick Rojas <qericrm@gmail.com>
2022-11-10 22:17:15 -0500

    news hero section made responsive

--- src/news/NewsContent.tsx
+++ src/news/NewsContent.tsx
@@ -3,15 +3,15 @@ import styles from './../styles/news/NewsContent.module.css'
 function News() : JSX.Element {
     return (
         <div className={styles.container}>
-            <section>
-                <h1>Latest News</h1>
-                <h1>Updates</h1>
-                <p>Look for something interesting to you,</p>
-                <input type="search" placeholder="Search"></input>
-                <img alt="bell image" src="./../../images/news/bell.png"/>
-                <img alt="light bulb image" src="./../../images/news/lightbulb.png"/>
-                <img alt="left decoration" src="./../../images/news/vector_left.png"/>
-                <img alt ="right decoration" src="./../../images/news/vector_right.png"/>
+            <section className={styles.hero}>
+                <h1>Latest news</h1>
+                <h1 className={styles.highlight}>Updates</h1>
+                <p>Look for something interesting to you.</p>
+                <input size={3} type="search" placeholder="Search"></input>
+                <img className={styles.bell} alt="bell" src="./../../images/news/bell.png"/>
+                <img className={styles.bulb} alt="light bulb" src="./../../images/news/lightbulb.png"/>
+                <img className={styles.leftDecoration} alt="left decoration" src="./../../images/news/vector_left.png"/>
+                <img className={styles.rightDecoration} alt ="right decoration" src="./../../images/news/vector_right.png"/>
             </section>
         </div>
     );
