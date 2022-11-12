6d697cca7b53d4d717797e215fd09bed64da4dde
Erick Rojas <qericrm@gmail.com>
2022-11-11 22:30:54 -0500

    added main News Section to news component

---
+++ src/Data.js
@@ -0,0 +1,38 @@
+export const data = [
+    {
+        "title" : "Lorem Ipsum is simply dummy text of the printing",
+        "image" : "images/news/news_11_11_22.png",
+        "summary" : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the .",
+        "content" : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the . More Content!",
+        "avatar" : "images/about/profile.jpg",
+        "author" : "Erick Rojas",
+        "date" : "November 11, 2022"
+    },
+    {
+        "title" : "Lorem Ipsum is simply dummy text of the printing",
+        "image" : "./../../images/..",
+        "summary" : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the .",
+        "content" : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the . More Content!",
+        "avatar" : "./../../images/",
+        "author" : "Erick Rojas",
+        "date" : "November 11, 2022"
+    },
+    {
+        "title" : "Lorem Ipsum is simply dummy text of the printing",
+        "image" : "./../../images/..",
+        "summary" : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the .",
+        "content" : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the . More Content!",
+        "avatar" : "./../../images/",
+        "author" : "Erick Rojas",
+        "date" : "November 11, 2022"
+    },
+    {
+        "title" : "Lorem Ipsum is simply dummy text of the printing",
+        "image" : "./../../images/..",
+        "summary" : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the .",
+        "content" : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the . More Content!",
+        "avatar" : "./../../images/",
+        "author" : "Erick Rojas",
+        "date" : "November 11, 2022"
+    }
+]
--- src/news/NewsContent.tsx
+++ src/news/NewsContent.tsx
@@ -1,17 +1,45 @@
+import {useState, useEffect} from 'react'
 import styles from './../styles/news/NewsContent.module.css'
+import {data} from './../Data';
 
 function News() : JSX.Element {
+
+    const [news, setNews] = useState<String>(" ");
+    useEffect(() => {
+        
+    },[]);
+
     return (
         <div className={styles.container}>
             <section className={styles.hero}>
                 <h1>Latest news</h1>
                 <h1 className={styles.highlight}>Updates</h1>
                 <p>Look for something interesting to you.</p>
-                <input size={3} type="search" placeholder="Search"></input>
+                <input className={styles.search} size={3} type="search" placeholder="Search"></input>
                 <img className={styles.bell} alt="bell" src="./../../images/news/bell.png"/>
                 <img className={styles.bulb} alt="light bulb" src="./../../images/news/lightbulb.png"/>
                 <img className={styles.leftDecoration} alt="left decoration" src="./../../images/news/vector_left.png"/>
                 <img className={styles.rightDecoration} alt ="right decoration" src="./../../images/news/vector_right.png"/>
+            </section>
+            <section className={styles.news}>
+                <div className={styles.lastNews}>
+                    <div className={styles.newsImage}>
+                        <img alt={data[0].title} src={data[0].image}/>
+                    </div>
+                    <div className={styles.newsDetails}>
+                        <h1>{data[0].title}</h1>
+                        <p>{data[0].summary}</p>
+                        <div className={styles.avatar}>
+                            <img alt="avatar" src={data[0].avatar}/>
+                            <div className={styles.avatarDetails}>
+                                <p>{data[0].author}</p>
+                                <p>{data[0].date}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.pastNews}>
+                </div>
             </section>
         </div>
     );
