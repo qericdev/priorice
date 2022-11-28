98ecddec4dc9e166b0bdb401f9020f44311f86e7
Erick Rojas <qericrm@gmail.com>
2022-11-28 11:53:00 -0500

    added form with post request in the front-end

--- src/generic/Subscribe.tsx
+++ src/generic/Subscribe.tsx
@@ -1,6 +1,6 @@
 import styles from './../styles/about/Subscribe.module.css';
 
-function Subscribe() : JSX.Element{
+function Subscribe() : JSX.Element {
     return (
         <section className={styles.subscribe}>
             <div className={styles.subscriptionBox}>
@@ -8,12 +8,10 @@ function Subscribe() : JSX.Element{
                     <h1>Stay in the loop</h1>
                     <p>Subscribe to receive the latest news and updates about TDA. We promise not to spam you! </p>
                 </div>
-                <div className={styles.register}>
-                    <input type="email" placeholder='Enter email address'></input>
-                    <button>Continue</button>
-                </div>
-                
-
+                <form className={styles.register} method='post' action=''>
+                    <input type="email" placeholder='Enter email address'/>
+                    <input onClick={(e) => e.preventDefault()} type="submit" value='Register'/>
+                </form>
             </div>
         </section>
     );
