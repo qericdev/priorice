2712839fff5b1c130e22097f1919bd46271d9787
Erick Rojas <qericrm@gmail.com>
2022-12-10 21:41:29 -0500

    recaptcha in about section make responsive

--- src/generic/Subscribe.tsx
+++ src/generic/Subscribe.tsx
@@ -1,6 +1,30 @@
 import styles from './../styles/about/Subscribe.module.css';
+import React, {useState, useEffect} from 'react';
+import {Alert} from 'react-bootstrap';
 
 function Subscribe() : JSX.Element {
+
+    const [formSubmitted, setFormSubmitted] = useState<boolean>(false);
+    const [recaptchaResponse, setRecaptchaResponse] = useState<String>("");
+
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = 'https://www.google.com/recaptcha/api.js';
+        script.async = true;
+        script.defer = true;
+        document.body.appendChild(script);
+    },[])
+
+    
+    function handleSubmit(event : React.FormEvent) {
+        event.preventDefault();
+        if (recaptchaResponse === "") {
+
+        } else {
+            setFormSubmitted(true);
+        }
+    }
+
     return (
         <section className={styles.subscribe}>
             <div className={styles.subscriptionBox}>
@@ -8,10 +32,20 @@ function Subscribe() : JSX.Element {
                     <h1>Stay in the loop</h1>
                     <p>Subscribe to receive the latest news and updates about TDA. We promise not to spam you! </p>
                 </div>
-                <form className={styles.register} method='post' action=''>
-                    <input type="email" placeholder='Enter email address'/>
-                    <input onClick={(e) => e.preventDefault()} type="submit" value='Register'/>
+                {
+                formSubmitted ?
+                <Alert variant="success" className={styles.alert}>
+                    Thanks for registering!
+                </Alert>
+                :
+                <form onSubmit={handleSubmit} className={styles.register} method='POST' action='https://8080-cs-955919841324-default.cs-us-east1-pkhd.cloudshell.dev/news'>
+                    <div className={styles.recaptcha}>
+                        <div className="g-recaptcha" data-size="compact" data-sitekey="6Ldo90ojAAAAAFnSCetk6G0viUXxyUX1GdfFKnPP" data-callback="handleOnLoad"></div>
+                    </div>
+                    <input name="email" id="email" type="email" placeholder='Enter email address'/>
+                    <input type="submit" value='Register'/>
                 </form>
+                }
             </div>
         </section>
     );
